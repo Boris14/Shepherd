29946a515434263790cfb569b7a445e90faad11e
Boris Dimitrov <boris1234dimitrov@gmail.com>
2025-01-07 00:45:03 +0200

    Created the World script and scene

---
+++ Scipts/World.gd
@@ -0,0 +1,13 @@
+class_name World
+extends Node2D
+
+@export var POI_count = 10
+@export var enemy_spawn_rate = 0.15
+@export var drone_sight_radius = 300.0
+@export var drones_count = 15
+
+
+func _ready():
+	# Use %DronesStart.position
+	# spawn drones at spawn point
+	pass
